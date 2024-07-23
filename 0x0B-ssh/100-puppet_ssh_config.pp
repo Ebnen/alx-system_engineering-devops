17dda916168183bea0b23016db4482eb8434e128
Ebnen <ebenzy1996@gmail.com>
2024-07-23 16:04:16 -0700

    ssf

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,9 @@
-!#/usr/bin/env bash
-# using a puppet to automate file 
+#!/usr/bin/env bash
+# SSH client configuration
 
-file {'/etc/ssh/ssh_config':
-  ensure  =>  'present',
-}
-
-file_line {'Turn off password auth':
-  Path    => '/etc/ssh/ssh_config',
-  Line    => 'PasswordAuthentication no',
-  Match   => 'PasswordAuthentication yes',
-  Replace => 'true',
-}
-
-File_line {'Declare Identity File':
-  ensure => 'present',
-  Path   => '/etc/ssh/ssh_config',
-  Line   => 'IdentityFile ~/.ssh/school',
-  Match  => '^IdentityFile',
-}
+Host *
+    PasswordAuthentication no
+    IdentityFile ~/.ssh/school
+    SendEnv LANG LC_*
+    HashKnownHosts yes
+    GSSAPIAuthentication yes
