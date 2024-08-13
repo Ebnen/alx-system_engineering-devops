28c5b3f64f303da8fad335b6a4917ff5ea1ab262
Ebnen <Ebenzy1996@gmail.com>
2024-08-13 18:44:49 +0100

    boy

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """funtion to write the following code"""
     url = f"https://api.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Mozilla/5.0', 'Accept': 'application/.json'}
     ok = requests.get(url, headers=headers, allow_redirects=False)
     subs = 0
     if ok.status_code == 200:
