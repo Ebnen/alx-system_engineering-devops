c1eb9ca3d6a6dd10f8ee35bf0279b7be67d891f3
Ebnen <Ebenzy1996@gmail.com>
2024-08-13 23:59:03 +0100

    boy

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,14 +5,14 @@ import requests
 
 def top_ten(subreddit):
     """a function that print out """
-    url = "https://www.reddit.com/r/{}/about.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code == 400:
+    if res.status_code == 200:
+        abga = res.json()
+        posts = abga['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
         print('None')
         return
-    else:
-        abga = res.json()
-        odas = abga['data']['children']
-        for oda in odas:
-            print(oda['data']['title'])
