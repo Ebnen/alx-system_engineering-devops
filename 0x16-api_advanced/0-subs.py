8847951f1ea3ea1f56451a97cdab3591370bfbc8
Ebnen <Ebenzy1996@gmail.com>
2024-08-15 18:57:12 +0100

    reet3w

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            rex = response.json()
-            data = rex['data'].get('subscribers', 0)
-            return data
-        else:
-            return 0
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        rex = response.json()
+        data = rex['data'].get('subscribers')
+        return data
+    else:
         return 0
-        
